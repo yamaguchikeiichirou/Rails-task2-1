1997f48a381f06812bcfbc77bd3874fbf6b8623a
EC2 Default User <ec2-user@ip-172-31-46-70.ap-northeast-1.compute.internal>
2022-11-06 05:17:02 +0000

    [Finish2-f]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice: 'Book was successfully created.'
+    else
+     @books = Book.all
+     render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     redirect_to book_path(@book.id),notice: "Book was successfully updated."
+    else
+
+     render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books',notice: "Book was successfully destroyed."
+  end
+
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
